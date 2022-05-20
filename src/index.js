ada431948080b77b7f0d2600de4146a42d7c39b0
Jackie <jye0325@gmail.com>
2022-05-19 23:14:20 -0400

    commit tree 24e3e36d74f8817d33cd35a2ec29edee00868a88

--- src/index.js
+++ src/index.js
@@ -1,5 +1,28 @@
 const init = () => {
-  
+    const inputForm = document.querySelector('form');
+
+    inputForm.addEventListener('submit', (event) => {
+        event.preventDefault();
+        // For debugging
+        // console.log(event);
+
+        // const input = event.target.children[1].value
+        // is an alternative to get the input value
+        const input = document.querySelector('input#searchByID');
+        // For debugging
+        // console.log(input.value);
+
+        fetch(`http://localhost:3000/movies/${input.value}`)
+        .then(response => response.json())
+        .then(data => {
+            const title = document.querySelector('section#movieDetails h4');
+            const summary = document.querySelector('section#movieDetails p');
+
+            title.innerText = data.title;
+            summary.innerText = data.summary;
+        });
+    });
 }
 
 document.addEventListener('DOMContentLoaded', init);
+
